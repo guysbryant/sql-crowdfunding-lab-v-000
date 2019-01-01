423ea03110b608dfa5d8c3d606a28099eda9791e
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 12:00:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -16,7 +16,7 @@ INSERT INTO users (id, name, age) VALUES
 (11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
 (17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
-INSERT INTO pledges (id, amount, project_id, user_id) VALUES 
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES 
 (1, 10.00, 1, 2),
 (2, 20.00, 1, 3),
 (3, 40.00, 1, 4),
