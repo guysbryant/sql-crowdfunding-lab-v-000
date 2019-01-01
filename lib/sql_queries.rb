19de2b4704ce6d6edcd10f3685b7d334c0deac1a
Guy Bryant <guy.s.bryant@gmail.com>
2019-01-01 11:47:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.amount 
-  FROM projects JOIN pledges ON pledges.projects_id = projects.id
+  FROM projects JOIN pledges ON pledges.project_id = projects.id
   GROUP BY title;"
 end
 
